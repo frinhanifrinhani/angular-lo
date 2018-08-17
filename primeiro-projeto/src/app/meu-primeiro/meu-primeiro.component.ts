50b44d2823ef31918a29263c234b993d6a4c42cc
frinhani.frinhani <frinhani.frinhani@gmail.com>
2018-08-16 23:29:58 -0300

    primeiro

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com angular2</p>
+    `
+})
+export class MeuPrimeiroComponent { }
