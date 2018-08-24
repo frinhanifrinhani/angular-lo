0163fa8b986c492643ac8ada42a133a2e7ebe5c4
frinhani.frinhani <frinhani.frinhani@gmail.com>
2018-08-23 22:48:58 -0300

    Aula 7

--- hello-typescript/main.js
+++ hello-typescript/main.js
@@ -5,8 +5,4 @@ function minhaFunc(x, y) {
 //ES6 ou ES 2015
 var num = 2;
 var PI = 3.14;
-var numeros = [1, 2, 3];
-numeros.map(function (valor) {
-    return valor * 2;
-});
-numeros.map(function (valor) { return valor * 2; }); //ES 2015
+
