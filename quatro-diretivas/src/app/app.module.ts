a4b8962eed7d4c305b89495d08bd892698db24c1
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-09 23:11:56 -0300

    Quatro diretivas finalização
    Quatro diretivas finalização

--- quatro-diretivas/src/app/app.module.ts
+++ quatro-diretivas/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { HighlightDirective } from './shared/highlight.directive';
+import { ngElseDirective } from './shared/ng-else.directive';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { HighlightDirective } from './shared/highlight.directive';
     FundoAmareloDirective,
     DiretivasCustomizadasComponent,
     HighlightMouseDirective,
-    HighlightDirective
+    HighlightDirective,
+    ngElseDirective
   ],
   imports: [
     BrowserModule
---
+++ quatro-diretivas/src/app/shared/ng-else.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ngElseDirective } from './ng-else.directive';
+
+describe('DiretivaNgElseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ngElseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
