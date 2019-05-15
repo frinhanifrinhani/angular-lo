d29b2b06fd6054e018ebb68f6897998ae475502b
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-14 23:11:57 -0300

    Seis Pipes
    46 - Pipes: Criando um Pipe "Puro"

--- seis-pipes/src/app/app.module.ts
+++ seis-pipes/src/app/app.module.ts
@@ -4,19 +4,24 @@ import { registerLocaleData } from '@angular/common'; //add para funcionar o loc
 import localePt from '@angular/common/locales/pt'; //add para funcionar o locale id
 registerLocaleData(localePt); //add para funcionar o locale id
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     //chamando pelo serviço
