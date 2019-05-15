d0807be1686b9b36e7d0aab309bf27b53dada9f6
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-14 22:26:52 -0300

    Seis Pipes
    45 - Aplicando Locale (internacionalização) nos Pipes

--- seis-pipes/src/app/app.module.ts
+++ seis-pipes/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common'; //add para funcionar o locale id
+import localePt from '@angular/common/locales/pt'; //add para funcionar o locale id
+registerLocaleData(localePt); //add para funcionar o locale id
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,21 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    //chamando pelo serviço
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+
+    //chamando direto
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
