be9843e5d97aab9e40a52d642806c8e0d7347edf
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-16 13:40:25 -0300

    Sete Rotas
    * Start project
    * Config Materialize - alternativo
    50 - Rotas: Configurando rotas simples

---
+++ sete-rotas/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
