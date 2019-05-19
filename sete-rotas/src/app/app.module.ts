0f7bdf40e09fc76266a1eb69ca6192ed5af09c9b
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-18 23:24:50 -0300

    Sete Rotas
    53 - Rotas: Definindo e extraindo parâmetros de roteamento

--- sete-rotas/src/app/app.module.ts
+++ sete-rotas/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { routing } from './app.routing';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
   BrowserModule,
