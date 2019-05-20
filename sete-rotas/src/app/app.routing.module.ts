8f9cffc5b2ae09a42ba22782b4016efe2cfa07f3
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-20 13:57:30 -0300

    Sete Rotas
    59 - Rotas: Criando um módulo de rotas de funcionalidade

--- sete-rotas/src/app/app.routing.module.ts
+++ sete-rotas/src/app/app.routing.module.ts
@@ -2,30 +2,16 @@ import { NgModule } from '@angular/core';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const appRoutes: Routes = [
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 ];
 
-
 @NgModule({
-  imports: [RouterModule.forRoot([
-      { path: 'cursos', component: CursosComponent },
-      { path: 'curso/:id', component: CursoDetalheComponent },
-      { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-      { path: 'login', component: LoginComponent },
-      { path: '', component: HomeComponent }
-    ])
-  ],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- sete-rotas/src/app/cursos/cursos.module.ts
+++ sete-rotas/src/app/cursos/cursos.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
+import { CursosRoutingModule } from './cursos.routing.model';
 
 
 @NgModule({
@@ -18,7 +18,7 @@ import { CursosService } from './cursos.service';
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule
+    CursosRoutingModule
   ],
   exports: [],
   providers: [CursosService],
