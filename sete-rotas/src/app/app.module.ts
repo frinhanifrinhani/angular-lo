2b9daff47e48b9a62ee04933944efee83fa89ac0
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-22 12:59:30 -0300

    Sete Rotas
    62 - Rotas: Dicas de Performance: Carregamento sob demanda (lazy loading)

--- sete-rotas/src/app/app.module.ts
+++ sete-rotas/src/app/app.module.ts
@@ -7,9 +7,7 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
-import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
@@ -20,8 +18,6 @@ import { AlunosModule } from './alunos/alunos.module';
   imports: [
     BrowserModule,
     FormsModule,
-    CursosModule,
-    AlunosModule,
 
     AppRoutingModule
   ],
