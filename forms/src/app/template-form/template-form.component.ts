b3b851f522b3d0e551b2d04fa2116f8fa50cce86
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-05-28 22:23:46 -0300

    Forms
    78 - Forms (template driven) Aplicando CSS na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Thiago',
-    email: 'frinhani@gmail.com'
+    /*nome: 'Thiago',
+    email: 'frinhani@gmail.com'*/
   }
   onSubmit(form){
     console.log(form.value);
