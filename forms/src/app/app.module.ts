74e2c6e81ce411d182a9af3f17f028cf8be3ab79
frinhani.frinhani <frinhani.frinhani@gmail.com>
2019-06-03 22:22:36 -0300

    Forms
    91 - Formulários reativos: Sincronizando HTML com FormGroup

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,26 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormDebugComponent } from './form-debug/form-debug.component';
+import { FormDebugComponent } from './shared/form-debug/form-debug.component';
 import { TemplateFormModule } from './template-form/template-form.module';
-import { CampoControlErrorComponent } from './campo-control-error/campo-control-error.component';
 import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //TemplateFormComponent,
-    DataFormComponent,
-    CampoControlErrorComponent
-    //FormDebugComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     TemplateFormModule,
     DataFormModule
   ],
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     DataFormComponent,
-    ReactiveFormsModule
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class DataFormModule { }
